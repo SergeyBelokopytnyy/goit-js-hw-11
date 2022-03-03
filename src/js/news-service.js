0e9f5cfbb735b4999b5be7a1f5a5792e00ea7d8f
Sergey Belokopytnyy <ser.oks.white@gmail.com>
2022-03-04 00:12:22 +0500

    Update news-service.js

--- src/js/news-service.js
+++ src/js/news-service.js
@@ -6,7 +6,7 @@ export default class NewsApiService {
   }
   fetchCards() {
     const URL = 'https://pixabay.com/api/';
-    const quantity = '5';
+    const quantity = '40';
     const KEY = '25851968-4919ec9d4a264dc8dd1f164b6';
     return fetch(
       `${URL}?key=${KEY}&q=${this.search}&per_page=${quantity}&orientation=horizontal&page=${this.page}&image_type=photo&safesearch=true`,
